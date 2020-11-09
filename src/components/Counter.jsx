d366bbf645d7a9fd7dbf7908d8506bea86481677
ross thompson <rossathompson94@gmail.com>
2020-11-09 14:12:04 +0000

    added a subtract count button to counter exercise

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,13 +5,14 @@ const Counter = () => {
   // useState used to set default state of count to 0 and create function for updating it
   const [currentCount, updateCurrentCount] = useState(0);
 
-  const updateCount = () => {
-    updateCurrentCount(currentCount + 1);
+  const updateCount = (type) => {
+    type === "add" ? updateCurrentCount(currentCount + 1) : updateCurrentCount(currentCount - 1);
   }
 
   return <div>
-    <button onClick={() => updateCount()}>Add to the count</button>
+    <button onClick={() => updateCount("add")}>Add to the count</button>
     <p>The current count is {currentCount}</p>
+    <button onClick={() => updateCount("subtract")}>Subtract from the count</button>
   </div>
 }
 
