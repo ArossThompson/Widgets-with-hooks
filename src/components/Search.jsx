6af6682da8e9784a81aa32ea61a23cd1206b56c5
ross thompson <rossathompson94@gmail.com>
2020-11-11 13:18:23 +0000

    implemented a delayed request to results search - 500ms delay before search term will be sent to API

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,8 +5,6 @@ const Search = () => {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
 
-  console.log(results);
-
   useEffect(() => {
     const search = async () => {
       const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
@@ -21,8 +19,14 @@ const Search = () => {
       setResults(data.query.search);
     }
 
-    if (term) {
-      search();
+    const timeoutId = setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 500)
+
+    return () => {
+      clearTimeout(timeoutId);
     }
   }, [term]);
  
