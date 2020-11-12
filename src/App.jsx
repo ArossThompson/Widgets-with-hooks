23b2d68f02ad6c0c427b13fd352d67e757f50a09
ross thompson <rossathompson94@gmail.com>
2020-11-12 09:06:52 +0000

    imported Dropdown component and added some app level state to handle the default selected option. passed useState setter in as a prop to be accessed inside component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // Components
 import Accordion from './components/Accordion';
 import ColourInput from './components/ColourInput';
 import Counter from './components/Counter';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 // Utils
-import { items } from './utils/constants'
-
-
-
-const colour = "Green";
+import { items, options } from './utils/constants'
 
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return <div>
-      <Search/>
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      />
     </div>;
 };
