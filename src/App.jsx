9b23abca7af7f45902649d346041657ae1b9c0d4
ross thompson <rossathompson94@gmail.com>
2020-11-14 11:20:46 +0000

    added a toggle button to hide and show the drop down with useState

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,17 @@ import { items, options } from './utils/constants'
 export default () => {
     const [selected, setSelected] = useState(options[0]);
 
+    const [dropdownOpen, setDropdownOpen] = useState(true);
+
     return <div>
-      <Dropdown
-        options={options}
-        selected={selected}
-        onSelectedChange={setSelected}
-      />
+      <button onClick={() => setDropdownOpen(!dropdownOpen)}>Toggle Dropdown</button>
+      
+      {dropdownOpen ?
+        <Dropdown
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        /> : null
+      } 
     </div>;
 };
