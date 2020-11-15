50ebe1603e14eab71b7624f0e03ff900db24110d
ross thompson <rossathompson94@gmail.com>
2020-11-15 10:41:25 +0000

    added a text input field to the translate component to later be used to convert

--- src/components/Translate.jsx
+++ src/components/Translate.jsx
@@ -6,10 +6,17 @@ import { languages } from '../utils/constants'
 
 const Translate = () => {
   const [language, setLanguage] = useState(languages[0]);
+  const [text, setText] = useState('');
 
 
   return (
       <div>
+        <div className="ui form">
+          <div className="field">
+            <label htmlFor="">Enter Text</label>
+            <input value={text} type="text" onChange={(e) => {setText(e.target.value)}}/>
+          </div>
+        </div>
         <Dropdown
           label="Select a language"
           options={languages}
