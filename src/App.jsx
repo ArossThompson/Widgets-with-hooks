dc5794af938f1b1d95e074ebbbd03cc49c2a3ff2
ross thompson <rossathompson94@gmail.com>
2020-11-18 07:58:26 +0000

    created some basic routing. intentionally not using react-router to get an understanding of what routing does

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,42 @@ import Counter from './components/Counter';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 // Utils
-import { items, options, languages } from './utils/constants'
+import { items, options } from './utils/constants'
+
+const showAccordion = () => {
+  if (window.location.pathname === "/accordion") {
+    return <Accordion items={items}/>
+  }
+}
 
 export default () => {
+    // Dropdown selection state
+    const [selected, setSelected] = useState(options[0]);
+
     return (
       <div>
-        <Translate />
+        <Header />
+        <Route path="/">
+          <Accordion items={items} />
+        </Route>
+        <Route path="/list">
+          <Search />
+        </Route>
+        <Route path="/dropdown">
+          <Dropdown
+            label="Select a colour"
+            options={options}
+            selected={selected}
+            onSelectedChange={setSelected}
+          />
+        </Route>
+        <Route path="/translate">
+          <Translate />
+        </Route>
       </div>
     );
 };
